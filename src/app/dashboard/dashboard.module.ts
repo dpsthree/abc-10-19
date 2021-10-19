0842628459ad52c1bc5c465ea7986ebf783ce331
Paul Spears <paul.spears@oasisdigital.com>
2021-10-19 16:13:39 -0500

    highlight video list background on click

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+
+const routes: Routes = [{ path: '', component: VideoDashboardComponent }];
+
+@NgModule({
+  declarations: [
+    VideoDashboardComponent,
+    VideoListComponent,
+    VideoPlayerComponent,
+  ],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class DashboardModule {}
